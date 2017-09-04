f0e42e8822f7b40f772d24244ac9a5341dbffb70
Linus-Smith <chyangbast@yeah.net>
2017-09-04 17:48:24 +0800

    add

--- opengesnative/src/main/jni/nativeDraw.cpp
+++ opengesnative/src/main/jni/nativeDraw.cpp
@@ -1,25 +1,10 @@
-#include <jni.h>
-#include <string>
-#include <android/log.h>
+
+#include "nativeDraw.h"
 
 
-static const char * kClassName = "com/example/opengesnative/MainActivity";
-
-#define  LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "linus" , __VA_ARGS__);
-
-
-void onSurfaceCreated();
-void onSurfaceChanged(JNIEnv env, jobject obj, int width, int height);
-void onDrawFrame();
-
-static const JNINativeMethod gMethod[] = {
-        {"onSurfaceCreated", "()V", (void *)onSurfaceCreated},
-        {"onSurfaceChanged",  "(II)V", (void *)onSurfaceChanged},
-        {"onDrawFrame", "()V", (void *)onDrawFrame}
-};
 
 void onSurfaceCreated() {
-    LOGD("onSurfaceCreated");
+   mProgram = esLoadProgram(vShader, fShader);
 }
 
 
@@ -28,29 +13,9 @@ void onSurfaceChanged(JNIEnv env, jobject obj, int width, int height) {
 }
 
 void onDrawFrame() {
-   // LOGD("onDrawFrddame");
+    // LOGD("onDrawFrddame");
 }
 
-JNIEXPORT jint JNI_OnLoad(JavaVM * vm, void * reserved) {
-    JNIEnv * env = NULL;
-
-    if(((*vm).GetEnv((void **)&env, JNI_VERSION_1_4)) != JNI_OK) {
-        return  -1;
-    }
-
-    jclass clazz = (*env).FindClass(kClassName);
-
-    if(clazz == NULL) {
-        LOGD("clazz == NULL")
-        return -1;
-    }
-
-    if((*env).RegisterNatives(clazz, gMethod, sizeof(gMethod) / sizeof(gMethod[0])) != JNI_OK) {
-        LOGD("register natives error");
-        return -1;
-    }
-    return JNI_VERSION_1_4;
-}
 
 
 
---
+++ opengesnative/src/main/jni/nativeDraw.h
@@ -0,0 +1,82 @@
+//
+// Created by a4004923 on 17-9-4.
+//
+
+#include <jni.h>
+#include <string>
+#include <android/log.h>
+#include <GLES/gl.h>
+#include "common/esUtil.h"
+
+#ifndef OPENGGL_ES_NATIVEDRAW_H
+#define OPENGGL_ES_NATIVEDRAW_H
+
+#endif //OPENGGL_ES_NATIVEDRAW_H
+
+
+static const char *kClassName = "com/example/opengesnative/MainActivity";
+
+#define  LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "linus" , __VA_ARGS__);
+
+
+GLint mProgram;
+
+void onSurfaceCreated();
+
+void onSurfaceChanged(JNIEnv env, jobject obj, int width, int height);
+
+void onDrawFrame();
+
+static const JNINativeMethod gMethod[] = {
+        {"onSurfaceCreated", "()V",   (void *) onSurfaceCreated},
+        {"onSurfaceChanged", "(II)V", (void *) onSurfaceChanged},
+        {"onDrawFrame",      "()V",   (void *) onDrawFrame}
+};
+
+
+
+static const char * vShader = {
+        "#version 300 es                  \n"
+                "uniform mat4 uMVPMatrix;"
+                "layout (location = 0) in vec3 aPosition;    \n"
+                "layout (location = 1) in vec4 aColor;       \n"
+                "out vec4 vColor;"
+                "void main() {                              \n"
+                "   gl_Position = uMVPMatrix * vec4(aPosition, 1); \n"
+                "   vColor = aColor;                                \n"
+                "}    \n"
+};
+
+
+static const char * fShader = {
+        "#version 300 es                                            \n"
+                "precision mediump float;                           \n"
+                "in vec4 vColor;                                     \n"
+                "out vec4 fragColor;                                  \n"
+                "void main(){                                          \n"
+                "    fragColor = vColor;                               \n"
+                " }"
+};
+
+
+
+JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+    JNIEnv *env = NULL;
+
+    if (((*vm).GetEnv((void **) &env, JNI_VERSION_1_4)) != JNI_OK) {
+        return -1;
+    }
+
+    jclass clazz = (*env).FindClass(kClassName);
+
+    if (clazz == NULL) {
+        LOGD("clazz == NULL")
+        return -1;
+    }
+
+    if ((*env).RegisterNatives(clazz, gMethod, sizeof(gMethod) / sizeof(gMethod[0])) != JNI_OK) {
+        LOGD("register natives error");
+        return -1;
+    }
+    return JNI_VERSION_1_4;
+}
